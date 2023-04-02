fdd33a4e6740d009c755cc82c8d962c18b8233ae
sumansauravmay <sumansauravmay12345@gmail.com>
2023-04-03 02:17:30 +0530

    Auto Play

---
+++ Day_1/autoplay/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AutoPlay from './component/AutoPlay';
+
+function App() {
+  return (
+    <div className="App">
+     <AutoPlay/>
+    </div>
+  );
+}
+
+export default App;
