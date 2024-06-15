724b3f31a0201c9ba5189c5bb07adec1a260edbe
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-16 02:37:29 +0530

    hof

--- HOF/index.js
+++ HOF/index.js
@@ -1,6 +1,15 @@
-let arr=[1,2,3,4,5];
+// HOF=> a funciton which take other function as an argument and return a function is called HOF.
 
+// Built in HOF=>
+// Map, foreach, filter and reduce (only for array)
 
+// map function=>return element in the array of the same size.
+
+// forEach=> return the value, not the value in the array.
+
+// filter=>return value in the array which satisfies condition.
+
+//reduce=> it summarize the total value of array and return that.
 
 
 
